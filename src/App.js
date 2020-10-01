d980158e8be86cfb76b74cffd939897d34a44dd0
Fernando Lopez <fernando.lopez@learnifier.com>
2020-10-01 12:32:07 +0200

    bootstrap login

--- src/App.js
+++ src/App.js
@@ -4,24 +4,51 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <form>
-        <div className="form-group">
-          <label htmlFor="exampleInputEmail1">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                 placeholder="Enter email"/>
-            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone
-              else.</small>
+      <div className="container">
+        <div className="d-flex justify-content-center h-100">
+          <div className="card">
+            <div className="card-header">
+              <h3>Sign In</h3>
+              <div className="d-flex justify-content-end social_icon">
+                <span><i className="fab fa-facebook-square"></i></span>
+                <span><i className="fab fa-google-plus-square"></i></span>
+                <span><i className="fab fa-twitter-square"></i></span>
+              </div>
+            </div>
+            <div className="card-body">
+              <form>
+                <div className="input-group form-group">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text"><i className="fas fa-user"></i></span>
+                  </div>
+                  <input type="text" className="form-control" placeholder="username"/>
+
+                </div>
+                <div className="input-group form-group">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text"><i className="fas fa-key"></i></span>
+                  </div>
+                  <input type="password" className="form-control" placeholder="password"/>
+                </div>
+                <div className="row align-items-center remember">
+                  <input type="checkbox"/>Remember Me
+                </div>
+                <div className="form-group">
+                  <input type="submit" value="Login" className="btn float-right login_btn"/>
+                </div>
+              </form>
+            </div>
+            <div className="card-footer">
+              <div className="d-flex justify-content-center links">
+                Don't have an account?<a href="#">Sign Up</a>
+              </div>
+              <div className="d-flex justify-content-center">
+                <a href="#">Forgot your password?</a>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="exampleInputPassword1">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
-        </div>
-        <div className="form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
+      </div>
     </div>
   );
 }
