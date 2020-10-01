77559e806d2449717b65f95d7f4ea373c03f6e47
Fernando Lopez <fernando.lopez@learnifier.com>
2020-10-01 12:37:36 +0200

    bootstrap login fix

--- src/App.js
+++ src/App.js
@@ -10,23 +10,27 @@ function App() {
             <div className="card-header">
               <h3>Sign In</h3>
               <div className="d-flex justify-content-end social_icon">
-                <span><i className="fab fa-facebook-square"></i></span>
-                <span><i className="fab fa-google-plus-square"></i></span>
-                <span><i className="fab fa-twitter-square"></i></span>
+                <span><i className="fab fa-facebook-square"/></span>
+                <span><i className="fab fa-google-plus-square"/></span>
+                <span><i className="fab fa-twitter-square"/></span>
               </div>
             </div>
             <div className="card-body">
               <form>
                 <div className="input-group form-group">
                   <div className="input-group-prepend">
-                    <span className="input-group-text"><i className="fas fa-user"></i></span>
+                    <span className="input-group-text">
+                      <i className="fas fa-user"/>
+                    </span>
                   </div>
                   <input type="text" className="form-control" placeholder="username"/>
 
                 </div>
                 <div className="input-group form-group">
                   <div className="input-group-prepend">
-                    <span className="input-group-text"><i className="fas fa-key"></i></span>
+                    <span className="input-group-text">
+                      <i className="fas fa-key"/>
+                    </span>
                   </div>
                   <input type="password" className="form-control" placeholder="password"/>
                 </div>
@@ -40,9 +44,11 @@ function App() {
             </div>
             <div className="card-footer">
               <div className="d-flex justify-content-center links">
-                Don't have an account?<a href="#">Sign Up</a>
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                Don't have an account? <a href="#">Sign Up</a>
               </div>
               <div className="d-flex justify-content-center">
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                 <a href="#">Forgot your password?</a>
               </div>
             </div>
